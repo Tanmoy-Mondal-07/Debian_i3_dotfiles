8c12df42adeb3cc0365d5c2bd66644aa3eccec66
Tanmoy-Mondal-07 <148357586+Tanmoy-Mondal-07@users.noreply.github.com>
2026-01-14 12:20:19 +0530

    Create NetworkManagerInstall.sh

---
+++ NetworkManagerInstall.sh
@@ -0,0 +1,13 @@
+sudo apt install NetworkManager
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+
+# remove ifupdown completely
+sudo apt purge ifupdown
+sudo apt autoremove
+
+# allow networkmanager to completely take over the role of ifupdown package
+sudo sed -i 's/^managed=false/managed=true/' /etc/NetworkManager/NetworkManager.conf
+
+# restart it
+sudo systemctl restart NetworkManager
